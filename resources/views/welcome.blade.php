2c4172a894832f095d135b330f79d799d5974a2b
Fabio Trani <trani.fabio.90@gmail.com>
2022-01-15 20:49:57 +0100

    create musthead

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,16 @@
 <x-layout>
-    hello world
+    <x-slot name='title'>
+        Fabio Trani Curriculum Vitae
+    </x-slot>
+
+
+    <div class="container">
+        <div class="row justify-content-center d-flex align-items-center">
+            <div class="row col-12 col-md-6 text-end">
+                <h1 class="col-12">Fabio Trani</h1>
+                <h3 class="col-12">Full-Stack Web Developer</h3>
+            </div>
+            <h2 class="col-12 col-md-6 text-start">Curriculum Vitae</h2>
+        </div>
+    </div>
 </x-layout>
