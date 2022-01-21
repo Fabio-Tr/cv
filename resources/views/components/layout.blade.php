0c4ba68efa025b46abf1ae7a1a85b0ec1770b6a3
Fabio Trani <trani.fabio.90@gmail.com>
2022-01-22 00:33:13 +0100

    easter egg

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,14 @@
 <html lang="it">
 
 <head>
+    
+███████╗  ████████╗
+██╔════╝  ╚══██╔══╝
+█████╗░░  ░░░██║░░░
+██╔══╝░░  ░░░██║░░░
+██║░░░░░  ░░░██║░░░
+╚═╝░░░░░  ░░░╚═╝░░░
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -24,6 +32,7 @@
 </head>
 
 <body>
+
 
 
     {{ $slot }}
