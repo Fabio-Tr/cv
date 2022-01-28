5f7a280a271e6cb9155f558650508ec014166894
Fabio Trani <trani.fabio.90@gmail.com>
2022-01-28 04:20:13 +0100

    domain

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
  --}}
     <div class="container-fluid footer justify-content-center d-flex mt-auto">
         <div class="row col-12 justify-content-center d-flex align-items-center">
-            <p class="col-6 yellow-text pe-2 text-end text-white">By <a href="https://www.knefas.eu"
+            <p class="col-6 yellow-text pe-2 text-end text-white">By <a href="https://www.fabiotrani.it"
                     class=""><img src="./img/Ft2.gif" alt="Il logo di Fabio Trani"
                         class="img-fluid"></a></p>
             <p class="col-6 text-start">
